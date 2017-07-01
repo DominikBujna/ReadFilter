c0db67445aaf0233644d6f6f00ac08f1ee6cd344
Dominik <dominikb94@gmail.com>
2017-07-01 20:23:12 +0200

    initial commit before debugging

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main() {
+    //minimal coverage and length to be filtered out
+    int minCoverage = 10;
+    int minLength = 200;
+
+    string line;
+    while (getline(cin, line))
+    {
+        if(line.at(0) == '>'){
+            //find last position of _ as it preceeds coverage number
+            size_t found = line.find_last_of('_');
+            //keep the rest of the line in order to extract the length
+            string restofline = line.substr(0, found);
+            int coverage = stoi(line.substr(found + 1));
+            //remove _ separating length value from cov
+            found = line.find_last_of('_');
+            restofline = line.substr(0, found);
+            //get seq length
+            found = line.find_last_of('_');
+            int length = stoi(restofline.substr(0, found));
+            //write out the line without > character
+            if(coverage < minCoverage || length < minLength){
+                cout<<line.substr(1);
+            }
+        }
+    }
+    return 0;
+}
